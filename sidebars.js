1e78642d0ceaf90bc3d588672f518692e0efea8e
Nicole Provenza <nicole_provenza@brown.edu>
2021-02-06 15:29:03 -0500

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,7 @@
 module.exports = {
     someSidebar: {
-        Overview: ["intro", "software_prerecs","getting_started"],
+        Overview: ["intro", "software_prerecs", "getting_started"],
+        HowTo: ["CI","event_triggers", "online_integration"],
         Design: ["folders"],
         Development: ["best_practices", "troubleshooting", "npm_scripts"],
     },
