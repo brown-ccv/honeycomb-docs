6cbc79b63606e31df60ca672efc2773fdb379f63
RobertGemmaJr <rgemma.jr@gmail.com>
2023-05-16 15:11:30 -0400

    chore: spelling

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -80,6 +80,7 @@ module.exports = {
           items: [
             {
               label:
+                // cspell:disable-next-line
                 'Provenza, N.R., Gelin, L.F.F., Mahaphanit, W., McGrath, M.C., Dastin-van Rijn, E.M., Fan, Y., Dhar, R., Frank, M.J., Restrepo, M.I., Goodman, W.K. and Borton, D.A., 2021. Honeycomb: a template for reproducible psychophysiological tasks for clinic, laboratory, and home use. Brazilian Journal of Psychiatry, 44, pp.147-155.',
               href: 'https://doi.org/10.1590/1516-4446-2020-1675',
             },
