7d622dfdb9c62d16a749b1c6fd267915e3aae641
RobertGemmaJr <rgemma.jr@gmail.com>
2024-04-24 13:15:46 -0400

    chore: comments

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,6 +24,7 @@ module.exports = {
 
           /**
            * Here we give a specific label to the current doc version. Note the old versions:
+           *    '3.4.x': { label: "3.3.x", path: "3.3.x" }
            *    '3.2.x': { label: "3.2.x", path: "3.2.x" }
            *    '3.1.x': { label: '3.1.x', path: '3.1.x' },
            *    '3.0.0': { label: '3.0.0', path: '3.0.0' },
