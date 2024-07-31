3797b0d3d9eea790401d2043d9ffa8b89d1cea88
YUUU23 <megan_zheng@brown.edu>
2024-07-31 15:12:30 -0500

    style: 2 columns for TOC

--- components/TableOfContents.jsx
+++ components/TableOfContents.jsx
@@ -9,81 +9,93 @@ import "../src/css/toc.css";
  */
 export default function TableOfContents() {
   return (
-    <div className="TOC">
+    <div className="TOC-container">
       <h2>Table of Contents</h2>
-      <ul>
-        <li>
-          <a href="../docs">Introduction</a>
-        </li>
-        <li>
-          <a href="../docs/quick_start">Quick Start</a>
-        </li>
-        <li>
-          <a href="../docs/prerequisites">Prerequisites</a>
-        </li>
-        <li>
-          Project Organization
+      <div className="TOC-content">
+        <div className="TOC-col">
           <ul>
             <li>
-              <a href="../docs/directory_structure">Directory Structure</a>
+              <a href="../docs">Introduction</a>
             </li>
             <li>
-              <a href="../docs/npm_scripts">NPM Scripts</a>
+              <a href="../docs/quick_start">Quick Start</a>
             </li>
             <li>
-              <a href="../docs/environment_variables">Environment Variables</a>
+              <a href="../docs/prerequisites">Prerequisites</a>
             </li>
             <li>
-              <a href="../docs/ci_cd">Continuous Integration / Deployment</a>
+              Project Organization
+              <ul>
+                <li>
+                  <a href="../docs/directory_structure">Directory Structure</a>
+                </li>
+                <li>
+                  <a href="../docs/npm_scripts">NPM Scripts</a>
+                </li>
+                <li>
+                  <a href="../docs/environment_variables">
+                    Environment Variables
+                  </a>
+                </li>
+                <li>
+                  <a href="../docs/ci_cd">
+                    Continuous Integration / Deployment
+                  </a>
+                </li>
+              </ul>
             </li>
           </ul>
-        </li>
-        <li>
-          Deployment
+        </div>
+        <div className="TOC-col">
           <ul>
             <li>
-              <a href="../docs/local_application">Local Application</a>
+              Deployment
+              <ul>
+                <li>
+                  <a href="../docs/local_application">Local Application</a>
+                </li>
+                <li>
+                  <a href="../docs/firebase">Firebase</a>
+                </li>
+                <li>
+                  <a href="../docs/psiturk">PsiTurk</a>
+                </li>
+                <li>
+                  <a href="../docs/gh_pages">GitHub Pages</a>
+                </li>
+              </ul>
             </li>
             <li>
-              <a href="../docs/firebase">Firebase</a>
+              External Tools
+              <ul>
+                <li>
+                  <a href="../docs/event_triggers">Event Triggers</a>
+                </li>
+                <li>
+                  <a href="../docs/prolific">Prolific</a>
+                </li>
+              </ul>
             </li>
             <li>
-              <a href="../docs/psiturk">PsiTurk</a>
+              Further Reading
+              <ul>
+                <li>
+                  <a href="../docs/github_discussions">Github Discussions</a>
+                </li>
+                <li>
+                  <a href="../docs/version_control">Version Control</a>
+                </li>
+                <li>
+                  <a href="../docs/javascript">JavaScript</a>
+                </li>
+              </ul>
             </li>
             <li>
-              <a href="../docs/gh_pages">GitHub Pages</a>
+              <a href="../docs/troubleshooting">Troubleshooting</a>
             </li>
           </ul>
-        </li>
-        <li>
-          External Tools
-          <ul>
-            <li>
-              <a href="../docs/event_triggers">Event Triggers</a>
-            </li>
-            <li>
-              <a href="../docs/prolific">Prolific</a>
-            </li>
-          </ul>
-        </li>
-        <li>
-          Further Reading
-          <ul>
-            <li>
-              <a href="../docs/github_discussions">Github Discussions</a>
-            </li>
-            <li>
-              <a href="../docs/version_control">Version Control</a>
-            </li>
-            <li>
-              <a href="../docs/javascript">JavaScript</a>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <a href="../docs/troubleshooting">Troubleshooting</a>
-        </li>
-      </ul>
+        </div>
+      </div>
     </div>
   );
 }
