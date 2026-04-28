bc786f02999ec6ebefd0c7f8f9e830f1f46c88c6
Heather Yu <35639529+hetd54@users.noreply.github.com>
2026-04-28 14:12:32 -0400

    feat: remove only beehive from current docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -74,7 +74,6 @@ module.exports = {
               label: "Discussions Board",
               href: "https://github.com/brown-ccv/honeycomb/discussions",
             },
-            { label: "Beehive", href: "https://beehive.ccv.brown.edu/" },
           ],
         },
         {
